62b17e7cc218ec35a81564e2509d6fea53abb706
Evan Laufer <evan.m.laufer@gmail.com>
2024-09-30 16:42:43 -0700

    test

--- halo2_proofs/src/plonk/circuit.rs
+++ halo2_proofs/src/plonk/circuit.rs
@@ -1792,7 +1792,7 @@ impl<F: Field> ConstraintSystem<F> {
     pub fn lookup(
         &mut self,
         // FIXME use name in debug messages
-        _name: &'static str,
+        _name: &str,
         table_map: impl FnOnce(&mut VirtualCells<'_, F>) -> Vec<(Expression<F>, TableColumn)>,
     ) {
         let mut cells = VirtualCells::new(self);
@@ -1939,7 +1939,7 @@ impl<F: Field> ConstraintSystem<F> {
     pub fn lookup_any(
         &mut self,
         // FIXME use name in debug messages
-        _name: &'static str,
+        _name: &str,
         table_map: impl FnOnce(&mut VirtualCells<'_, F>) -> Vec<(Expression<F>, Expression<F>)>,
     ) {
         let mut cells = VirtualCells::new(self);
